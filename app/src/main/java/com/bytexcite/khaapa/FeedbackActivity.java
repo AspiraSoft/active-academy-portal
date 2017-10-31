11138dc0fb3bc199deebc9c6bc6553317c5836bf
Muhammad Saifullah Khan <sfllhkhan95@live.com>
2017-11-01 02:57:43 +0500

    Implemented feedback feature

--- app/src/main/java/com/bytexcite/khaapa/FeedbackActivity.java
+++ app/src/main/java/com/bytexcite/khaapa/FeedbackActivity.java
@@ -2,12 +2,56 @@ package com.bytexcite.khaapa;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+import com.bytexcite.khaapa.models.Feedback;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class FeedbackActivity extends AppCompatActivity {
+
+    private EditText emailView;
+    private EditText feedbackView;
+
+    private RatingBar ratingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+
+        emailView = (EditText) findViewById(R.id.email);
+        feedbackView = (EditText) findViewById(R.id.message);
+
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        ratingBar.setRating(2.5f);
+
+        findViewById(R.id.submit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailView.getText().toString().replace("\n", "").trim();
+                String feedback = feedbackView.getText().toString().replace("\n", "").trim();;
+
+                if (!email.endsWith("@seecs.edu.pk")) {
+                    Toast.makeText(FeedbackActivity.this, "Invalid email address.", Toast.LENGTH_SHORT).show();
+                }
+
+                else if (email.equals("") || feedback.equals("")) {
+                    Toast.makeText(FeedbackActivity.this, "All fields are required.", Toast.LENGTH_SHORT).show();
+                }
+
+                else {
+                    Feedback.submit(FeedbackActivity.this, new Feedback(email, feedback, ratingBar.getRating()), new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(FeedbackActivity.this, "Feedback submitted.", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    });
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/bytexcite/khaapa/models/Feedback.java
@@ -0,0 +1,44 @@
+package com.bytexcite.khaapa.models;
+
+import android.app.Activity;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class Feedback {
+
+    private String sender;
+    private String content;
+    private Float rating;
+
+    public Feedback() {
+        // Default constructor required for calls to DataSnapshot.getValue(Feedback.class)
+    }
+
+    public Feedback(String sender, String content, Float rating) {
+        this.sender = sender;
+        this.content = content;
+        this.rating = rating;
+    }
+
+    public Float getRating() {
+        return rating;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public static void submit(Activity activity, Feedback feedback, OnSuccessListener<Void> listener) {
+        FirebaseDatabase.getInstance()
+                .getReference("feedback").push()
+                .setValue(feedback)
+                .addOnSuccessListener(activity, listener);
+    }
+
+}
